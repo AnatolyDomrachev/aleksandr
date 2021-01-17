1629bc732d781b6a199a71ac4d971ce0a1be9790
Ваше Имя <you@example.com>
2021-01-17 19:03:47 +0300

    1

--- 2.b1.py
+++ 2.b1.py
@@ -3,14 +3,14 @@
 Дана сторона квадрата a. Найти его периметр P = 4·a.
 '''
 
-while True:
+while True: # Цикл
     a = input("введите а: ")
-    if a.isdigit():
+
+    if a.isdigit(): # Условие
         a = int(a)
         break
     else:
             print("Вы ввели не число, повторите ввод")
-
 
 p = 4*a
 print("Периметр = ", p);
---
+++ f.b1.py
@@ -0,0 +1,24 @@
+# Begin 1 С функциями
+'''
+Дана сторона квадрата a. Найти его периметр P = 4·a.
+'''
+
+def vvod():
+    global a
+    a = input("введите а: ")
+
+def proverka():
+    global a
+    print(a)
+
+def raschet():
+    pass
+
+def vyvod():
+    pass
+
+a = ''
+vvod()
+proverka()
+raschet()
+vyvod()
